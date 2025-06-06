18e00cceb19fcd0a8090275657d82225431c173d
feitozabruno <109050932+feitozabruno@users.noreply.github.com>
2025-06-06 17:27:29 +0000

    feat: create local `database`

---
+++ infra/database.js
@@ -0,0 +1,35 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Erro no banco de dados: ", error);
+    throw new Error("Erro na conexão com Banco ou na Query.");
+  } finally {
+    await client?.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+  return client;
+}
+
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
